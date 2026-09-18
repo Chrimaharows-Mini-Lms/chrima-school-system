00f5a58ebb890b2387f730e8edbc789d9f4103e6
yosefsahle <yosefshale48@gmail.com>
2026-09-18 01:14:27 -0300

    test email content updated

--- application/controllers/School_settings.php
+++ application/controllers/School_settings.php
@@ -907,7 +907,7 @@ class School_settings extends Admin_Controller
                 $data = array();
                 $data['branch_id'] = $branchID;
                 $data['recipient'] = $recipient;
-                $data['subject'] = 'Ramom School SMTP Config Testing';
+                $data['subject'] = 'Zohar School Management SMTP Config Testing';
                 $data['message'] = 'This is test SMTP config email. <br />If you received this message that means that your SMTP settings is set correctly.';
                 $r = $this->mailer->send($data, true);
                 if ($r == "true") {
